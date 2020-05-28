ce4ce8ba8f96ede54ee271a2225a12561c860010
mseeli <markus@seeli.eu>
2020-05-28 12:38:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello CodeSandbox Local</h1>
       <h2>
         Start editing to see some magic happen! And here are some Changes from
         Sandbox Some More Changes and some more. And now some local changes.
